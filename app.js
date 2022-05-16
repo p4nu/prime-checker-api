c26063a0379baa53908421263b27720891dca856
Panu Valtanen <p4nu@users.noreply.github.com>
2022-05-16 15:11:49 +0300

    Remove unnecessary boilerplate

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const checkPrimeRouter = require('./routes/checkPrime');
 const sumAndCheckRouter = require('./routes/sumAndCheck');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/checkPrime', checkPrimeRouter);
 app.use('/api/sumAndCheck', sumAndCheckRouter);
 
